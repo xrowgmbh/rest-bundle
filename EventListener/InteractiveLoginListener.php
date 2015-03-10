6b9ebf6e75a0f69a87a5538f8be45fe5317b83ca
xrowkristina <kristina@xrow.de>
2015-03-10 15:41:21 +0100

    bugfixing

--- EventListener/InteractiveLoginListener.php
+++ EventListener/InteractiveLoginListener.php
@@ -1,30 +1,30 @@
 <?php
 namespace xrow\restBundle\EventListener;
- 
+
 use eZ\Publish\API\Repository\UserService;
 use eZ\Publish\Core\MVC\Symfony\Event\InteractiveLoginEvent;
 use eZ\Publish\Core\MVC\Symfony\MVCEvents;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
- 
+
 class InteractiveLoginListener implements EventSubscriberInterface
 {
     /**
      * @var \eZ\Publish\API\Repository\UserService
      */
     private $userService;
- 
+
     public function __construct( UserService $userService )
     {
         $this->userService = $userService;
     }
- 
+
     public static function getSubscribedEvents()
     {
         return array(
             MVCEvents::INTERACTIVE_LOGIN => 'onInteractiveLogin'
         );
     }
- 
+
     public function onInteractiveLogin( InteractiveLoginEvent $event )
     {
         // We just load a generic user and assign it back to the event.
