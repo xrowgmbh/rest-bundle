4ffbde121b81499d1c0ed99ff37ea8b147b684d8
xrowkristina <kristina@xrow.de>
2015-03-10 15:59:17 +0100

    tests

--- DependencyInjection/xrowRestExtension.php
+++ DependencyInjection/xrowRestExtension.php
@@ -24,8 +24,10 @@ class xrowRestExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
+        #$loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        #$loader->load('services.yml');
+        $loaderXml = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loaderXml->load('services.xml');
     }
 
     /**
