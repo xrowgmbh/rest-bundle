b24bd5a0301d8ee56fbe33240802ba1b9389903f
xrowkristina <kristina@xrow.de>
2015-03-17 13:33:36 +0100

    added required file

--- Command/CreateClientCommand.php
+++ Command/CreateClientCommand.php
@@ -1,24 +1,14 @@
 <?php
 
-/*
- * This file is part of the Sylius package.
- *
- * (c) Paweł Jędrzejewski
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace xrow\restBundle\Command;
 
 use FOS\OAuthServerBundle\Model\ClientManagerInterface;
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class CreateClientCommand extends Command
+class CreateClientCommand extends ContainerAwareCommand
 {
     protected function configure()
     {
