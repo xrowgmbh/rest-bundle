7fe7b5745f152ccda6bae60164a7952bdd748a9f
xrowkristina <kristina@xrow.de>
2015-04-02 15:20:32 +0200

    create a shell test oauth2 client and an oauth2 client

--- Command/CredentialsCommand.php
+++ Command/CredentialsCommand.php
@@ -2,7 +2,6 @@
 
 namespace xrow\restBundle\Command;
 
-use OAuth2;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -19,24 +18,35 @@ class CredentialsCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $username = $this->getContainer()->getParameter('oauth2_username');
-        $password = $this->getContainer()->getParameter('oauth2_password');
-        $parameters = array('username' => $username, 'password' => $password);
-        $request = new Request(array(), $parameters);
-        $OAuth2Client = $this->getContainer()->get('xrow_rest.oauth2.client');
-        $accessToken = $OAuth2Client->requestAccessTokenWithUserCredentials($request);
-
-        // bearer
-        //$credentialsClient->setAccessTokenAsBearer();
-        $output->writeln(sprintf('Obtained Access Token: <info>%s</info>', $accessToken));
-        $url = 'http://abo.example.com/xrowapi/user';
-        $parameters = array('access_token' => $accessToken,
-                            'response_type' => 'token',
-                            'grant_type' => 'client_credentials',
-                            'scope' => 'user');
-        $output->writeln(sprintf('Requesting: <info>%s</info>', $url));
-        $output->writeln(sprintf('Parameters: <info>%s</info>', var_export($parameters, true)));
-        $response = $credentialsClient->fetch($url, $parameters);
-        $output->writeln(sprintf('Response: <info>%s</info>', var_export($response, true)));
+        try {
+            // in shell you can't get http host
+            $url = $this->getContainer()->getParameter('host_url');
+            $username = $this->getContainer()->getParameter('oauth2_username');
+            $password = $this->getContainer()->getParameter('oauth2_password');
+            $parameters = array('username' => $username, 'password' => $password, 'url' => $url);
+            $request = new Request(array(), $parameters);
+            $request->setMethod('POST');
+            $OAuth2Client = $this->getContainer()->get('xrow_rest.oauth2.client');
+            $accessTokenArray = $OAuth2Client->requestAccessTokenWithUserCredentials($request);
+            $accessToken = $accessTokenArray['access_token'];
+            $output->writeln(sprintf('Obtained Access Token: <info>%s</info>', $accessToken));
+            $output->writeln('');
+            $parameters = array('access_token' => $accessToken,
+                                'grant_type' => 'client_credentials');
+            // User data
+            $userDataUri = '/xrowapi/user';
+            $output->writeln(sprintf('Get user data: <info>%s</info>', $userDataUri));
+            $userDataResponse = $OAuth2Client->getApiDataWithPath($userDataUri, $accessToken);
+            $output->writeln(sprintf('Response: <info>%s</info>', var_export($userDataResponse, true)));
+            // Subscription data
+            $userDataSubscrUri = '/xrowapi/user/subscriptions';
+            $output->writeln(sprintf('Get user subscriptions: <info>%s</info>', $userDataSubscrUri));
+            $userDataSubscrResponse = $OAuth2Client->getApiDataWithPath($userDataSubscrUri, $accessToken);
+            $output->writeln(sprintf('Response: <info>%s</info>', var_export($userDataSubscrResponse, true)));
+        }
+        catch (Exception $e)
+        {
+            die(var_dump($e->getMessage()));
+        }
     }
 }
--- Service/OAuth2Client.php
+++ Service/OAuth2Client.php
@@ -4,13 +4,17 @@ namespace xrow\restBundle\Service;
 
 use Symfony\Component\HttpFoundation\Request;
 use OAuth2\OAuth2;
+use OAuth2\OAuth2Client as FOSOAuth2Client;
 use OAuth2\OAuth2ServerException;
+use OAuth2\OAuth2Exception;
+use Symfony\Component\DependencyInjection\Exception\InvalidArgumentException;
 
 class OAuth2Client
 {
     protected $token_uri;
     protected $client_id;
     protected $client_secret;
+    protected $currentUrl;
     /**
      * @var OAuth2
      */
@@ -22,56 +26,179 @@ class OAuth2Client
         $this->token_uri = $params['token_uri'];
         $this->client_id = $params['client_id'];
         $this->client_secret = $params['client_secret'];
-        #$this->container = twig
     }
 
-    public function getAuthenticationUrl()
+    public function getCurrentUrl($url = '')
     {
-        return $this->client->getAuthenticationUrl($this->authEndpoint, $this->redirectUrl, array(''));
+        if($this->currentUrl === null)
+        {
+            if($url != '')
+                $this->currentUrl = $url;
+            else 
+            {
+                $protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on'
+                    ? 'https://'
+                    : 'http://';
+                $server_host = $_SERVER['HTTP_HOST'];
+                $current_uri = $protocol . $server_host;
+                $parts = parse_url($current_uri);
+                // Use port if non default.
+                $port = isset($parts['port']) &&
+                (($protocol === 'http://' && $parts['port'] !== 80) || ($protocol === 'https://' && $parts['port'] !== 443))
+                    ? ':' . $parts['port'] : '';
+                // Rebuild.
+                $this->currentUrl = $protocol . $parts['host'] . $port;
+            }
+        }
+        return $this->currentUrl;
     }
 
+    /**
+     * 
+     * @param Request $request
+     * @throws InvalidArgumentException
+     * @throws OAuth2Exception
+     * @return array|null A valid OAuth2.0 JSON decoded access token in associative array, and null if not enough parameters or JSON decode failed.
+     */
     public function requestAccessTokenWithUserCredentials(Request $request)
     {
         try {
-            if ($request->getMethod() === 'POST')
-                $parameters = $request->request->all();
-            else 
-                $parameters = $request->query->all();
-            $parameters['client_id'] = $this->client_id;
-            $parameters['client_secret'] = $this->client_secret;
-die(var_dump($request->server->all()));
-            $response = Request::create($this->token_uri, $request->getMethod(), $parameters, array(), array(), $request->server->all());
-            
-            $accessTokenJson = $this->server->grantAccessToken($request);
-            $json = json_decode((string)$accessTokenJson, true);
-            die(var_dump($json));
-            if (isset($json['access_token'])) {
-                // weiter zu /xrowapi/user
-                return $json['access_token'];
-                #return $twig->render('client/show_access_token.twig', array('response' => $json));
+            if ($this->token_uri && $this->client_id && $this->client_secret) {
+                if ($request->getMethod() === 'POST')
+                    $parameters = $request->request->all();
+                else 
+                    $parameters = $request->query->all();
+                if(isset($parameters['url']))
+                    $url = $this->getCurrentUrl($parameters['url']);
+                else
+                    $url = $this->getCurrentUrl();
+                $fullTokenURL = $url . $this->token_uri;
+                $params = array('grant_type' => 'password',
+                                'client_id' => $this->client_id,
+                                'client_secret' => $this->client_secret,
+                                'username' => $parameters['username'],
+                                'password' => $parameters['password']);
+                $accessTokenCall = json_decode($this->makeRequest($fullTokenURL, 'POST', $params), true);
+                if (isset($accessTokenCall['access_token'])) {
+                    return $accessTokenCall;
+                }
+                else {
+                    throw new InvalidArgumentException('not enough parameters in accessTokenCall, ' . __METHOD__);
+                }
             }
-            #return $twig->render('client/failed_token_request.twig', array('response' => $json ? $json : $response));
-        } catch (OAuth2ServerException $e) {
-            return $e->getHttpResponse();
+            else {
+                throw new InvalidArgumentException('token_uri, client_id and client_secret have to set in ' . __METHOD__);
+            }
+        } catch (OAuth2Exception $e) {
+            throw new OAuth2Exception($e->__toString());
         }
-        // exchange user credentials for access token
-        $query = array(
-            'grant_type' => 'password',
-            'client_id' => $config['client_id'],
-            'client_secret' => $config['client_secret'],
-            'username' => $username,
-            'password' => $password,
-        );
-        // determine the token endpoint to call based on our config (do this somewhere else?)
-        $grantRoute = $config['token_route'];
-        $endpoint = 0 === strpos($grantRoute, 'http') ? $grantRoute : $urlgen->generate($grantRoute, array(), true);
-        // make the token request via http and decode the json response
-        $response = $http->post($endpoint, null, $query, $config['http_options'])->send();
-        $json = json_decode((string) $response->getBody(), true);
-        // if it is succesful, display the token in our app
-        if (isset($json['access_token'])) {
-        return $twig->render('client/show_access_token.twig', array('response' => $json));
+    }
+
+    public function getApiDataWithPath($path, $accessToken, $method = 'GET')
+    {
+        $fullTokenURL = $this->getCurrentUrl() . $path;
+        $params = array('access_token' => $accessToken,
+                        'grant_type' => 'client_credentials');
+        $jsonDataCall = json_decode($this->makeRequest($fullTokenURL, $method, $params), true);
+        if (count($jsonDataCall) > 0) {
+            return $jsonDataCall;
         }
-        return $twig->render('client/failed_token_request.twig', array('response' => $json ? $json : $response));
+        else {
+            throw new InvalidArgumentException('not enough parameters in accessTokenCall, ' . __METHOD__);
+        }
+    }
+
+    /**
+     * Makes an HTTP request.
+     *
+     * This method can be overriden by subclasses if developers want to do
+         * fancier things or use something other than cURL to make the request.
+     *
+     * @param string        $path   The target path, relative to base_path/service_uri or an absolute URI.
+     * @param string        $method (optional) The HTTP method (default 'GET').
+     * @param array         $params (optional The GET/POST parameters.
+     * @param resource|null $ch     (optional) An initialized curl handle
+     *
+     * @throws OAuth2Exception
+     * @return string The JSON decoded response object.
+     */
+    protected function makeRequest($path, $method = 'GET', $params = array(), $ch = null)
+    {
+        if (!$ch) {
+            $ch = curl_init();
+        }
+
+        $opts = FOSOAuth2Client::$CURL_OPTS;
+        if ($params) {
+            switch ($method) {
+                case 'GET':
+                    $path .= '?' . http_build_query($params, null, '&');
+                    break;
+                    // Method override as we always do a POST.
+                default:
+                    $opts[CURLOPT_POSTFIELDS] = http_build_query($params, null, '&');
+            }
+        }
+        $opts[CURLOPT_URL] = $path;
+
+        // Disable the 'Expect: 100-continue' behaviour. This causes CURL to wait
+        // for 2 seconds if the server does not support this header.
+        if (isset($opts[CURLOPT_HTTPHEADER])) {
+            $existing_headers = $opts[CURLOPT_HTTPHEADER];
+            $existing_headers[] = 'Expect:';
+            $opts[CURLOPT_HTTPHEADER] = $existing_headers;
+        } else {
+            $opts[CURLOPT_HTTPHEADER] = array('Expect:');
+        }
+
+        curl_setopt_array($ch, $opts);
+        $result = curl_exec($ch);
+
+        if (curl_errno($ch) == 60) { // CURLE_SSL_CACERT
+            error_log('Invalid or no certificate authority found, using bundled information');
+            curl_setopt(
+            $ch,
+            CURLOPT_CAINFO,
+            dirname(__FILE__) . '/fb_ca_chain_bundle.crt'
+                    );
+                    $result = curl_exec($ch);
+        }
+
+        if ($result === false) {
+            $e = new OAuth2Exception(array(
+                    'code' => curl_errno($ch),
+                    'message' => curl_error($ch),
+            ));
+            curl_close($ch);
+            throw $e;
+        }
+        curl_close($ch);
+
+        // Split the HTTP response into header and body.
+        list($headers, $body) = explode("\r\n\r\n", $result);
+        $headers = explode("\r\n", $headers);
+        // We catch HTTP/1.1 4xx or HTTP/1.1 5xx error response.
+        if (strpos($headers[0], 'HTTP/1.1 4') !== false || strpos($headers[0], 'HTTP/1.1 5') !== false) {
+            $result = array(
+                    'code' => 0,
+                    'message' => '',
+            );
+
+            if (preg_match('/^HTTP\/1.1 ([0-9]{3,3}) (.*)$/', $headers[0], $matches)) {
+                $result['code'] = $matches[1];
+                $result['message'] = $matches[2];
+            }
+
+            // In case retrun with WWW-Authenticate replace the description.
+            foreach ($headers as $header) {
+                if (preg_match("/^WWW-Authenticate:.*error='(.*)'/", $header, $matches)) {
+                    $result['error'] = $matches[1];
+                }
+            }
+
+            return json_encode($result);
+        }
+
+        return $body;
     }
 }
