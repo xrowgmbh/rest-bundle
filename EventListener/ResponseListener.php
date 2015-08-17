ba7b97869697254baccbbbd44dc963111a283497
xrowkristina <kristina@xrow.de>
2015-08-17 11:08:51 +0200

    added if for set required headers only in routings of xrowapi and oauth

--- EventListener/ResponseListener.php
+++ EventListener/ResponseListener.php
@@ -11,21 +11,18 @@ class ResponseListener
         $response = $event->getResponse();
         $responseHeaders = $response->headers;
         $requestHeaders = $request->headers;
-        if (strpos($request->getRequestUri(), '/xrowapi/v1/') !== false || strpos($request->getRequestUri(), '/oauth/v2/') !== false || strpos($request->getRequestUri(), '/sso/') !== false || $request->getRequestUri() == '/sso') {
-            if (strpos($request->getRequestUri(), '/xrowapi/v1/') !== false || strpos($request->getRequestUri(), '/oauth/v2/') !== false) {
-                $responseHeaders->set('Access-Control-Allow-Headers', 'Content-type');
-                if ($requestHeaders->get('Origin')) {
-                    $responseHeaders->set('Access-Control-Allow-Origin', $requestHeaders->get('Origin'));
-                }
-                else {
-                    $responseHeaders->set('Access-Control-Allow-Origin', '*');
-                }
-                $responseHeaders->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PATCH, DELETE');
-                $responseHeaders->set('Access-Control-Allow-Credentials', 'true');
+        if (strpos($request->getRequestUri(), '/xrowapi/v1/') !== false || strpos($request->getRequestUri(), '/oauth/v2/') !== false) {
+            $responseHeaders->set('Access-Control-Allow-Headers', 'Content-type');
+            if ($requestHeaders->get('Origin')) {
+                $responseHeaders->set('Access-Control-Allow-Origin', $requestHeaders->get('Origin'));
             }
-            if (strpos($request->getRequestUri(), '/xrowapi/v1/') !== false || strpos($request->getRequestUri(), '/sso/') !== false || $request->getRequestUri() == '/sso') {
+            else {
+                $responseHeaders->set('Access-Control-Allow-Origin', '*');
+            }
+            $responseHeaders->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PATCH, DELETE');
+            $responseHeaders->set('Access-Control-Allow-Credentials', 'true');
+            if (strpos($request->getRequestUri(), '/xrowapi/v1/') !== false) {
                 $responseHeaders->addCacheControlDirective('no-cache', true);
-                $responseHeaders->addCacheControlDirective('max-age', 0);
                 $responseHeaders->addCacheControlDirective('must-revalidate', true);
                 $responseHeaders->addCacheControlDirective('no-store', true);
             }
