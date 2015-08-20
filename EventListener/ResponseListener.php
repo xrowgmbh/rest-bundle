77abd93060976b32c625348a4728cb34e4f17fa5
xrowkristina <kristina@xrow.de>
2015-08-20 14:53:32 +0200

    reverted changes

--- EventListener/ResponseListener.php
+++ EventListener/ResponseListener.php
@@ -11,14 +11,21 @@ class ResponseListener
         $response = $event->getResponse();
         $responseHeaders = $response->headers;
         $requestHeaders = $request->headers;
-        $responseHeaders->set('Access-Control-Allow-Headers', 'Content-type');
-        if ($requestHeaders->get('Origin')) {
-            $responseHeaders->set('Access-Control-Allow-Origin', $requestHeaders->get('Origin'));
+        if (strpos($request->getRequestUri(), '/xrowapi/v1/') !== false || strpos($request->getRequestUri(), '/oauth/v2/') !== false) {
+            $responseHeaders->set('Access-Control-Allow-Headers', 'Content-type');
+            if ($requestHeaders->get('Origin')) {
+                $responseHeaders->set('Access-Control-Allow-Origin', $requestHeaders->get('Origin'));
+            }
+            else {
+                $responseHeaders->set('Access-Control-Allow-Origin', '*');
+            }
+            $responseHeaders->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PATCH, DELETE');
+            $responseHeaders->set('Access-Control-Allow-Credentials', 'true');
+            $response->setPrivate();
+            $response->setMaxAge(0);
+            $response->setSharedMaxAge(0);
+            $response->mustRevalidate();
+            $responseHeaders->addCacheControlDirective('no-store', true);
         }
-        else {
-            $responseHeaders->set('Access-Control-Allow-Origin', '*');
-        }
-        $responseHeaders->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PATCH, DELETE');
-        $responseHeaders->set('Access-Control-Allow-Credentials', 'true');
     }
 }
